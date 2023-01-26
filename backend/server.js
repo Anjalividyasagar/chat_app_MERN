34ab8c33d248461ae22315e5709b1ab6109de75e
Anjalividyasagar <anjali130btece20@igdtuw.ac.in>
2023-01-26 20:56:08 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,9 @@ const __dirname1 =path.resolve();
 if(process.env.NODE_ENV==="production"){
     console.log(__dirname1)
   
-    app.use(express.static(path.join("frontend","build")));
+    app.use(express.static(path.join(__dirname1,"/frontend/build")));
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"frontend","build","index.html"));
+        res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
     });
 } else{
     app.get("/",(req,res)=>{
