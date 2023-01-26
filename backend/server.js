d98c42e688ce958d9bebbc0c02f45cab636bb3e2
Anjalividyasagar <anjali130btece20@igdtuw.ac.in>
2023-01-26 20:20:24 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -22,20 +22,20 @@ app.use('/api/chat',chatRoutes);
 app.use("/api/message", messageRoutes);
 //--------------------deployment------------------------
 
-// const __dirname1 =path.resolve();
-// if(process.env.NODE_ENV==="production"){
-//     console.log("production")
+const __dirname1 =path.resolve();
+if(process.env.NODE_ENV==="production"){
+    console.log("production")
   
-//     app.use(express.static(path.join(__dirname1,"frontend","build")));
-//     app.get('*',(req,res)=>{
-//         res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
-//     });
-// } else{
-//     app.get("/",(req,res)=>{
-//         res.send("API is running successfully");
+    app.use(express.static(path.join(__dirname1,"frontend","build")));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+    });
+} else{
+    app.get("/",(req,res)=>{
+        res.send("API is running successfully");
 
-//     })
-// }
+    })
+}
 
 //--------------------deployment------------------------
 
